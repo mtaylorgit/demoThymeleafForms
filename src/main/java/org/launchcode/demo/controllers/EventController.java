7adf8487c19efd2988e7f9935506c469d113d28d
oggie <morkthealien@gmail.com>
2023-10-02 13:57:58 -0500

    updated next step

--- src/main/java/org/launchcode/demo/controllers/EventController.java
+++ src/main/java/org/launchcode/demo/controllers/EventController.java
@@ -1,34 +1,40 @@
+
 package org.launchcode.demo.controllers;
 
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Goop");
-        events.add("Apple Joke");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
-
     }
 
-    //lives at /events
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm() {
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 
 }
